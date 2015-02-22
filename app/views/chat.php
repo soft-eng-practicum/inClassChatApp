26dea81b3b3b6daa192f2b39473ca27875744b33
Jake <jakemny@gmail.com>
2015-02-22 18:15:06 -0500

    include css

--- app/views/chat.php
+++ app/views/chat.php
@@ -19,8 +19,8 @@
 
   <!-- CSS
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="/bulletin/app/views/static/normalize.css">
+  <link rel="stylesheet" href="/bulletin/app/views/static/skelaton.css">
 
   <!-- Favicon
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
