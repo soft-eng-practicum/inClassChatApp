f867990273af9ef9008bebbbfac233eb6c501330
Jake <jakemny@gmail.com>
2015-03-05 16:06:21 -0500

    created models in models.php

--- backend/app/models.php
+++ backend/app/models.php
@@ -16,4 +16,36 @@ class User extends Model {
 
 */
 
+class User extends Model {
+	public $email;
+	public $name;
+	public $domain;
+	public $school_id;
+	public $password;
+	public $createdAt;
+	public $session_id;
+}
+
+class School extends Model {
+	public $domain;
+	public $name;
+}
+
+class Course extends Model {
+	public $name;
+	public $school_id;
+}
+
+class Enrollment extends Model {
+	public $user_id; 
+	public $course_id; 
+}
+
+class Message extends Model {
+	public $content;
+	public $likes;
+	public $createdAt;
+	public $owner_id;
+	public $course_id;
+}
 ?>
