b9048c979af517218aab3be5ca496eb17299b2e1
Jack Barnes <jfbarnes5@gmail.com>
2015-03-29 17:52:50 -0400

    added Registration screen
    functional front end

--- ClassChatAndroid/app/build/generated/source/r/debug/com/example/TodoList/R.java
+++ ClassChatAndroid/app/build/generated/source/r/debug/com/example/TodoList/R.java
@@ -19,12 +19,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_add_task=0x7f070006;
-        public static final int action_settings=0x7f070007;
+        public static final int action_add_task=0x7f070009;
+        public static final int action_settings=0x7f07000a;
+        public static final int btnLinkToLoginScreen=0x7f070008;
         public static final int btnLinkToRegisterScreen=0x7f070005;
         public static final int btnLogin=0x7f070004;
+        public static final int btnRegister=0x7f070007;
         public static final int doneButton=0x7f070001;
         public static final int email=0x7f070002;
+        public static final int name=0x7f070006;
         public static final int password=0x7f070003;
         public static final int taskTextView=0x7f070000;
     }
@@ -32,14 +35,16 @@ public final class R {
         public static final int activity_comments=0x7f030000;
         public static final int activity_login=0x7f030001;
         public static final int activity_questions=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int task_view=0x7f030004;
+        public static final int activity_register=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int task_view=0x7f030005;
     }
     public static final class menu {
         public static final int menu=0x7f060000;
         public static final int menu_comments=0x7f060001;
         public static final int menu_login=0x7f060002;
         public static final int menu_questions=0x7f060003;
+        public static final int menu_register=0x7f060004;
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
@@ -48,5 +53,6 @@ public final class R {
         public static final int title_activity_comments=0x7f050003;
         public static final int title_activity_login=0x7f050004;
         public static final int title_activity_questions=0x7f050005;
+        public static final int title_activity_register=0x7f050006;
     }
 }
