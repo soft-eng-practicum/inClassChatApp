ba02293dfc3d29fb9bdf6b034dde02aef24a22fd
Jack Barnes <jfbarnes5@gmail.com>
2015-03-29 15:30:31 -0400

    Android update
    added login page, classes, questions, comments, layout creation, icon,
    local DB created for testing

---
+++ ClassChatAndroid/app/src/main/java/com/example/TodoList/db/CommentContract.java
@@ -0,0 +1,15 @@
+package com.example.TodoList.db;
+
+import android.provider.BaseColumns;
+
+public class CommentContract {
+    public static final String DB_NAME = "com.example.TodoList.db.comments";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE = "tasks";
+
+
+    public class Columns {
+        public static final String TASK = "task";
+        public static final String _ID = BaseColumns._ID;
+    }
+}
